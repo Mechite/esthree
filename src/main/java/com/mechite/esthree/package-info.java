e4e0deaea0f87bbae0ae946a125d5777e287b09d
Mechite <contact@mechite.com>
2025-10-04 06:41:54 +0100

    Initial commit (3)
    Signed-off-by: Mechite <contact@mechite.com>

--- src/main/java/com/mechite/esthree/package-info.java
+++ src/main/java/com/mechite/esthree/package-info.java
@@ -1,3 +1,10 @@
+/**
+ * Tiny Java S3 client using avaje-http-client & JDK's built-in XML parsing.
+ * Very small, secure and trivial. Requires Java >11.
+ * Virtual Threads compatible with Java >21.
+ *
+ * @see com.mechite.esthree.Esthree
+ */
 @NullMarked
 package com.mechite.esthree;
 
